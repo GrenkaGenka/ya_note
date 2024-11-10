aa20c0280d77db5f2cc72769fcf7deeb94210a77
Genka <Gennadiusisus@gmil.com>
2024-11-10 23:47:42 +0700

    final_project

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from notes.models import Note
 
+
 User = get_user_model()
 
 
@@ -20,19 +21,21 @@ class TestRoutes(TestCase):
             slug='one_note'
         )
 
+
+    def test_home_page(self):
+        url = reverse('notes:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    
     def test_pages_availability(self):
             # Создаём набор тестовых данных - кортеж кортежей.
             # Каждый вложенный кортеж содержит два элемента:
             # имя пути и позиционные аргументы для функции reverse().
-            urls = (
-                ('notes:home', None),
-                ('notes:detail', (self.note.slug,)),
+            urls = (                
                 ('notes:add', None),
                 ('notes:list', None),
-                ('notes:success', None),
-                ('users:login', None),
-                ('users:logout', None),
-                ('users:signup', None),
+                ('notes:success', None),           
             )
             # Итерируемся по внешнему кортежу 
             # и распаковываем содержимое вложенных кортежей:
@@ -46,7 +49,7 @@ class TestRoutes(TestCase):
                     self.assertEqual(response.status_code, HTTPStatus.OK) 
 
 
-    def test_availability_for_comment_edit_and_delete(self):
+    def test_availability_for_note_edit_and_delete(self):
         users_statuses = (
             (self.author, HTTPStatus.OK),
             (self.reader, HTTPStatus.NOT_FOUND),
@@ -56,26 +59,51 @@ class TestRoutes(TestCase):
             self.client.force_login(user)
             # Для каждой пары "пользователь - ожидаемый ответ"
             # перебираем имена тестируемых страниц:
-            for name in ('notes:edit', 'notes:delete'):
+            for name in ('notes:edit', 'notes:delete', 'notes:detail'):
                 with self.subTest(user=user, name=name):
                     url = reverse(name, args=(self.note.slug,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
-    
 
-    def test_redirect_for_anonymous_client(self):
-        # Сохраняем адрес страницы логина:
+
+    def test_redirect_to_login(self):
+        urls = (                
+                ('notes:add', None),
+                ('notes:list', None),
+                ('notes:add', None),
+                ('notes:detail', self.note.slug,),
+                ('notes:edit', self.note.slug,),
+                ('notes:delete', self.note.slug,),
+            )
+        
         login_url = reverse('users:login')
         # В цикле перебираем имена страниц, с которых ожидаем редирект:
-        for name in ('notes:edit', 'notes:delete'):
+        for name, arg in urls:
             with self.subTest(name=name):
                 # Получаем адрес страницы редактирования или удаления комментария:
-                url = reverse(name, args=(self.note.slug,))
-                # Получаем ожидаемый адрес страницы логина, 
-                # на который будет перенаправлен пользователь.
-                # Учитываем, что в адресе будет параметр next, в котором передаётся
-                # адрес страницы, с которой пользователь был переадресован.
+                if arg is None:
+                    url = reverse(name)
+                else:
+                    url = reverse(name, args=(arg,))
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url) 
+
+
+    def test_availability_auth_pages_for_all_users(self):
+        # Создаём набор тестовых данных - кортеж кортежей.
+        # Каждый вложенный кортеж содержит два элемента:
+        # имя пути и позиционные аргументы для функции reverse().
+        urls = (                
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),        
+        )
+        # Итерируемся по внешнему кортежу 
+        # и распаковываем содержимое вложенных кортежей:
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK) 
